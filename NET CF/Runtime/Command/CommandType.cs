8902a8ece2bf65dbcae770e3c479b3d793a7eb86
cKey <35512826+colinin@users.noreply.github.com>
2019-02-23 17:15:11 +0800

    Create CommandType.cs

---
+++ NET CF/Runtime/Command/CommandType.cs
@@ -0,0 +1,30 @@
+````C#
+namespace Runtime.Command
+{
+    /// <summary>
+    /// 执行命令类别
+    /// </summary>
+    public enum CommandType
+    {
+        /// <summary>
+        /// 无操作
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// 通知
+        /// </summary>
+        Notification = 2,
+        /// <summary>
+        /// 文件操作
+        /// </summary>
+        File = 4,
+        /// <summary>
+        /// 应用程序相关
+        /// </summary>
+        Application = 8,
+        /// <summary>
+        /// 系统相关
+        /// </summary>
+        System = 16
+    }
+}
