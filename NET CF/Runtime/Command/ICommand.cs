9c979cd5daf917456b27e815b3abf85be10c4320
cKey <35512826+colinin@users.noreply.github.com>
2019-02-23 17:14:16 +0800

    Create ICommand.cs

---
+++ NET CF/Runtime/Command/ICommand.cs
@@ -0,0 +1,27 @@
+````C#
+using System;
+namespace Runtime.Command.Command
+{
+    /// <summary>
+    /// 命令接口
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// 是否帮助命令
+        /// </summary>
+        bool IsHelp { get; }
+        /// <summary>
+        /// 帮助信息
+        /// </summary>
+        string Help { get; }
+        /// <summary>
+        /// 执行命令
+        /// </summary>
+        void Call();
+        /// <summary>
+        /// 执行完毕事件
+        /// </summary>
+        EventHandler<CommandEventArgs> ExecutedHandler { get; set; }
+    }
+}
